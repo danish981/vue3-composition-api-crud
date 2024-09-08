5477b3f77fe684188855a85dfb214ff986ad7e78
danish <danishmehmood981@gmail.com>
2024-09-08 03:12:32 -0700

    adding a new student done

--- src/composables/studentApi.js
+++ src/composables/studentApi.js
@@ -3,11 +3,25 @@ import axios from "axios";
 
 // write compostable function to get all students, the function name should be useStudents, `use(EntityName)`
 export default function useStudents() {
+  // todo : make the url dynamic by adding the port number or endpoint in a seperate file
   const url = "http://localhost:4000/students/";
   const students = ref([]);
   const error = ref(null);
 
-  // get all students
+  const validateIfEmailAlreadyExists = () => {
+    //todo : add validattions for the email
+  };
+
+  // todo :we will add the username in the json after, we will expand this application later
+  const createUsernameForStudent = (name) => {
+    // todo : logic for createUsername for student
+    // add logic for creating a username for a student from provided name
+    // search for existing names in the db
+    // if name found seearch for count, if found, increment the counter and concatenate with name and return the username string
+    // if not found, return the name as the username
+  };
+
+  // get / fetch all students record
   const getAllStudents = async () => {
     students.value = []; // what if the students are already loaded and it has some data, then it will be replaced by an empty array
     error.value = null;
@@ -22,6 +36,7 @@ export default function useStudents() {
     }
   };
 
+  // fetch / get record of a single student
   const getSingleStudentData = async (id) => {
     students.value = [];
     error.value = null;
@@ -33,13 +48,49 @@ export default function useStudents() {
       return response.data;
     } catch (exceptionError) {
       console.log(exceptionError);
+      error.value = exceptionError
     }
   };
 
+  // adding a new student
+  const addNewStudent = async (formData) => {
+    students.value = [];
+    error.value = null;
+
+    try {
+      const config = {
+        method: "POST",
+        url: url,
+        headers: {
+          "Content-type": "Application/json",
+        },
+        data: JSON.stringify(formData),
+      };
+
+      const response = await axios(config);
+      // todo: create validation that name cannot be duplciated if editied or added somehow
+
+      students.value = response.data;
+
+      console.log(response);
+      return students.value;
+    } catch (exceptionError) {
+      console.log(exceptionError);
+      error.value = exceptionError
+    }
+  };
+
+  // updating a student
+  const editStudentData = async (id) => {
+    // logic for updating a student by searching the record wrt id provided
+  };
+
   return {
-    students, // we have used the single variable for single student data and all students data
+    students, // everything we call a method, we store the response into this reactive variable and fetch this from the import from component
     error,
     getAllStudents,
     getSingleStudentData,
+    addNewStudent,
+    editStudentData,
   };
 }
